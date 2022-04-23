426645bd6cb6e354b65f01abc1e4bb1b4a4f4dd9
Safikul Islam <islam.safikul@hotmail.com>
2022-04-23 19:49:13 +0530

    6_strings.py

---
+++ 7_strings.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python
+# 6_strings.py
+
+a = "Safikul Islam"
+b = 'Bellal Hossain'
+c = """
+My
+sister's
+name
+is
+Sumi Parvin
+"""
+print(a)
+print(b)
+print(c)
+print("bbb\raaa")
+print(" bbb\raaa")
+print("  bbb\raaa")
+print("   bbb\raaa")
+print("Safikul has", len("safikul"), "charecters")
